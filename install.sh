9f94ff91398b7f624bcbf844bc07b24fa7d47edb
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2021-12-12 18:47:26 +0000

    Patch for ARM64 compilation of dtksh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
-# $Id: install.sh 10 2021-01-11 21:09:06Z rhubarb-geek-nz $
+# $Id: install.sh 12 2021-01-14 23:36:51Z rhubarb-geek-nz $
 #
 
 if test -n "$1"
@@ -45,6 +45,32 @@ git clone git://git.code.sf.net/p/cdesktopenv/code cdesktopenv-code
 	cd cdesktopenv-code
 	git checkout "$VERSION"
 	cd cde
+	patch -p0 <<'EOF'
+--- programs/dtksh/ksh93/src/lib/libast/sfio/sfvprintf.c	2021-01-14 17:13:54.191401167 +0000
++++ programs/dtksh/ksh93/src/lib/libast/sfio/sfvprintf.c	2021-01-14 17:19:54.178126980 +0000
+@@ -92,10 +92,10 @@
+ 	}
+ #define GETARGL(elt,arge,argf,args,etype,type,fmt,t_user,n_user) \
+ 	{ if(!argf) \
+-		__va_copy( elt, va_arg(args,type) ); \
++		__va_copy( elt[0], va_arg(args,type)[0] ); \
+ 	  else if((*argf)(fmt,(char*)(&arge),t_user,n_user) < 0) \
+ 		goto pop_fa; \
+-	  else	__va_copy( elt, arge ); \
++	  else	__va_copy( elt[0], arge[0] ); \
+ 	}
+ 
+ #if __STD_C
+@@ -309,7 +309,7 @@
+ #else
+ 			GETARGL(argsp,argsp,argf,args,va_list*,va_list*,'2',t_user,n_user);
+ 			__va_copy( fa->args, args );
+-			__va_copy( args, argsp );
++			__va_copy( args, argsp[0] );
+ #endif
+ 			fa->argf.p = argf;
+ 			fa->extf.p = extf;
+EOF
 	make World
 )
 
